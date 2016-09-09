9ea4cc9ea1150af9f02bc1c33fc3eca1cc394b94
amcginlay <amcginlay@gmail.com>
2016-09-09 12:07:51 +0100

    Initial Commit

---
+++ src/HelloWorld.Test/ProgramTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using HelloWorld;
+
+namespace HelloWorld.Test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        private class TestWriter : TextWriter
+        {
+            public String Value { get; private set; }
+
+            public override System.Text.Encoding Encoding
+            {
+                get { return System.Text.Encoding.Unicode; }
+            }
+
+            public override void WriteLine(string value) {
+                Value = value;
+            }
+        }
+        
+        [TestMethod]
+        public void Test_Start_WriterObjectYieldsTextHelloWorld_WhenInvoked()
+        {
+            // arrange
+            var textWriter = new TestWriter();
+            var program = new Program(textWriter);
+            // act
+            program.Start();
+            // assert
+            Assert.AreEqual("Hello, World!", textWriter.Value);
+        }
+    }
+}
---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program(Console.Out).Start();
+        }
+
+        private System.IO.TextWriter textWriter;
+
+        public Program(System.IO.TextWriter textWriter)
+        {
+            this.textWriter = textWriter;
+        }
+
+        public void Start()
+        {
+            textWriter.WriteLine("Hello, World!");
+        }
+    }
+}
